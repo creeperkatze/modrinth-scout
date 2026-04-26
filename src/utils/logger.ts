054fc1aa0c849b0798e9d4a7bda81c54654b00f1
Creeperkatze <contact@creeperkatze.dev>
2026-04-26 22:50:40 +0200

    Use level label in pino

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,6 +2,9 @@ import pino from 'pino'
 
 export const logger = pino({
 	level: process.env.LOG_LEVEL ?? 'info',
+	formatters: {
+		level: (label) => ({ level: label }),
+	},
 	...(process.env.NODE_ENV !== 'production' && {
 		transport: {
 			target: 'pino-pretty',
