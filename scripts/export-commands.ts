57e8d892808961ad2b99b7e576f409cd6138c45d
Creeperkatze <contact@creeperkatze.dev>
2026-07-04 14:49:56 +0200

    Add export commands script

---
+++ scripts/export-commands.ts
@@ -0,0 +1,12 @@
+import { writeFileSync } from 'node:fs'
+
+import { commands } from '../src/commands/index.js'
+import { createCommandRegistry } from '../src/utils/commands.js'
+
+const { getAllSlashCommandData } = createCommandRegistry(commands)
+const data = getAllSlashCommandData()
+
+const out = 'commands.json'
+writeFileSync(out, JSON.stringify(data, null, 2))
+
+console.log(`Wrote ${data.length} commands to ${out}`)
