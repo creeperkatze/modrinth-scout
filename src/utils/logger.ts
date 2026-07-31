e9f325e5336ae841316e295222476e5a218fb0d6
Creeperkatze <contact@creeperkatze.dev>
2026-07-31 20:37:58 +0200

    Hide pid from logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -7,7 +7,7 @@ export const logger = pino({
 	},
 	transport: {
 		target: 'pino-pretty',
-		options: { colorize: true, singleLine: true },
+		options: { colorize: true, singleLine: true, ignore: 'pid,hostname' },
 	},
 })
 
